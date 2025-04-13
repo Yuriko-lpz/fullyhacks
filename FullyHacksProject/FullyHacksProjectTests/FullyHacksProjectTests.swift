8cdf72219dc3e02476aaeafb3345b92c7b437ee6
Elena Marquez <elenavm724@gmail.com>
2025-04-12 20:01:40 -0700

    added code actually

---
+++ FullyHacksProject/FullyHacksProjectTests/FullyHacksProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  FullyHacksProjectTests.swift
+//  FullyHacksProjectTests
+//
+//  Created by Elena Marquez on 4/12/25.
+//
+
+import Testing
+@testable import FullyHacksProject
+
+struct FullyHacksProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
